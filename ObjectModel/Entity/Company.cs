3230904c8a1657da3e909331c9c4ec71d2ad55db
DangThanh2001 <thanhsunflower2001@gmail.com>
2023-03-02 17:23:55 +0700

    add entity

---
+++ ObjectModel/Entity/Company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectModel.Entity
+{
+    internal class Company
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
