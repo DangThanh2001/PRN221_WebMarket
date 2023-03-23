e5ad4ebde23a383f85b7f2ad0d70a8c299357dd7
UsagiYuki2312 <usagiyuki.nana@gmail.com>
2023-03-23 21:22:08 +0700

    CRUD

--- Service/ProductService.cs
+++ Service/ProductService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DataAccess.IRepositotry;
+using ObjectModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,37 @@ using System.Threading.Tasks;
 
 namespace Service
 {
-    internal class ProductService
+    public class ProductService
     {
+        private IProductRepository _repository;
+        public ProductService(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Product> GetAllProduct()
+        {
+            return _repository.GetAllProduct();
+        }
+
+        public Product GetProductWithId(int id)
+        {
+            return _repository.GetProductWithId(id);
+        }
+
+        public void AddProduct(Product product)
+        {
+            _repository.AddProduct(product);
+        }
+
+        public void UpdateCategory(Product product)
+        {
+            _repository.UpdateProduct(product);
+        }
+
+        public void DeleteCategory(int id)
+        {
+            _repository.DeleteProduct(id);
+        }
     }
 }
