67922a40fb70687e7924a79a20db02bdd0deede3
oxit Nhôm <luutiendat12072001@gmail.com>
2023-03-25 00:16:40 +0700

    back end lisst category

--- Web_Market/Pages/Category_List.cshtml.cs
+++ Web_Market/Pages/Category_List.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ObjectModel;
+using Service;
+using static Microsoft.Extensions.Logging.EventSource.LoggingEventSource;
 
 namespace Web_Market.Pages
 {
-    public class Category_ListModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class Category_ListModel : PageModel
+	{
+		private readonly CategoryService _categoryService;
+		[BindProperty]
+		public Category _category { get; set; }
+		[BindProperty]
+		public List<Category>? listCategories { get; set; }
+		public Category_ListModel(CategoryService category)
+		{
+			_categoryService = category;
+		}
+
+		public void OnGet(string? category_name_key)
+		{
+			listCategories = _categoryService.GetAllCategory();
+			if (!string.IsNullOrWhiteSpace(category_name_key) && !string.IsNullOrEmpty(category_name_key))
+			{
+				listCategories = listCategories.Where(x => x.CategoryName
+				.ToLower().Contains(category_name_key.ToLower().Trim())
+				).ToList();
+			}
+			ViewData["search"] = category_name_key;
+
+		}
+	}
 }
