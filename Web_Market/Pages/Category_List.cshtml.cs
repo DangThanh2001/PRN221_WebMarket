5360f3df4ef7327220815cfd63b52486f80d5f57
oxit Nhôm <luutiendat12072001@gmail.com>
2023-03-25 01:09:54 +0700

    Back end category list

--- Web_Market/Pages/Category_List.cshtml.cs
+++ Web_Market/Pages/Category_List.cshtml.cs
@@ -9,28 +9,40 @@ namespace Web_Market.Pages
 	public class Category_ListModel : PageModel
 	{
 		private readonly CategoryService _categoryService;
+		private readonly ProductService _productService;
+
 		[BindProperty]
 		public Category _category { get; set; }
+		public Product? product { get; set; }
+		public List<Product> listProduct { get; set; }
+
 
 		[BindProperty]
 		public List<Category>? listCategories { get; set; }
 		public Category_ListModel(CategoryService category, ProductService productService)
 		{
 			_categoryService = category;
+			_productService = productService;
 		}
 
-		public void OnGet(string? category_name_key)
+		public void OnGet(string? product_name_key, string? category_id_key)
 		{
 			listCategories = _categoryService.GetAllCategory();
-			if (!string.IsNullOrWhiteSpace(category_name_key) && !string.IsNullOrEmpty(category_name_key))
+			listProduct = _productService.GetAllProduct();
+			if (!string.IsNullOrWhiteSpace(product_name_key) && !string.IsNullOrEmpty(product_name_key))
 			{
-				listCategories = listCategories.Where(x => x.CategoryName
-				.ToLower().Contains(category_name_key.ToLower().Trim())
+				listProduct = listProduct.Where(x => x.ProductName
+				.ToLower().Contains(product_name_key.ToLower().Trim())
 				).ToList();
 			}
-			
+			if (!string.IsNullOrWhiteSpace("3") && !string.IsNullOrEmpty("3"))
+			{
+				listProduct = listProduct.Where(x => x.ProductCategory
+				.ToLower().Contains("3".ToLower().Trim())
+				).ToList();
+			}
 
-			ViewData["search"] = category_name_key;
+			ViewData["search"] = product_name_key;
 
 		}
 	}
